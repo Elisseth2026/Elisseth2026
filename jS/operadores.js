3398f62e18a8572b950b9aaaf579d398f8ad3cdd
Elisseth2026 <19783857@clases.edu.sv>
2026-08-24 10:03:56 -0600

    segunda prueva

---
+++ jS/operadores.js
@@ -0,0 +1,8 @@
+alert("Bienvenidos a ejemplos de operadores aritméticos");
+var numero1=15;
+var numero2=25;
+
+alert("El valor del primer número es: "+numero1);
+alert("El valor del segundo número es: "+numero2);
+var suma=numero1+numero2;
+alert("La suma es igual a: "+suma);
